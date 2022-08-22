983ff543ae958fab23786285b9653d857f53cc44
ChandanGupta <ChandanGupta@INP-PF3B0L19-LT.tsys.tss.net>
2022-08-22 22:10:11 +0530

    My first commit

---
+++ src/college/classes/std11/HelloWord.java
@@ -0,0 +1,23 @@
+package college.classes.std11;
+
+public class HelloWord {
+	
+	int variablename = 10;
+	String place = "India";
+	
+
+	public static void main(String[] args) {
+		
+          System.out.println("Hello World");
+	}
+	
+	public static void  add() {
+		
+	}
+}
+
+// Class is blueprint for an Object OR a blueprint to create an object
+
+// Human as a template
+
+// We all are the object made out of that template
---
+++ src/college/classes/std12/Abc.java
@@ -0,0 +1,14 @@
+package college.classes.std12;
+
+public class Abc {
+	
+	static Dog labrador = new Dog();
+	static Dog GoldenR = new Dog();
+	
+	public static void main(String[] args) {
+		
+		System.out.println(labrador);
+		System.out.println(GoldenR);
+	}
+	
+}
